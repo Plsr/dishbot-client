2f8b1bfa31716c32df3415cd04cb511437df0aa8
Plsr <christianpoplawski@gmail.com>
2022-03-19 23:37:25 +0100

    Basic sign in

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,15 +1,67 @@
+import { useState } from "react";
 import { signInWithEmailAndPassword } from "firebase/auth"
 import auth from '../util/firebase'
-import { Button } from '@chakra-ui/react';
+import { Button, Input, useToast, Box, Center, Heading } from '@chakra-ui/react';
 
 export default function SignIn() {
-  // TODO: Handle 4xx errors
+  const toast = useToast()
+  const [emailValue, setEmailValue] = useState('')
+  const [passwordValue, setPasswordValue] = useState('')
+
+  const handleEmailChange = (event) => setEmailValue(event.target.value)
+  
+  const handlePasswordChange = (event) => setPasswordValue(event.target.value)
+
   const handleLoginButtonPress = async () => {
-    console.log(auth)
-    const authRes = await signInWithEmailAndPassword(auth, 'test3@example.com', process.env.REACT_APP_FIREBASE_DUMMY_PW)
+    try {
+      await signInWithEmailAndPassword(auth, emailValue, passwordValue)
+    } catch (error) {
+      toast({
+        title: 'Something went wrong',
+        description: "Please make sure you entered the correct email and password",
+        status: 'error',
+        duration: 6000,
+        isClosable: true,
+      })
+      console.error(error)
+    }
   }
 
   return (
-    <Button onClick={handleLoginButtonPress}>Login</Button>
+    <Center>
+      <Box
+        mt={20}
+        p={6} 
+        maxW='lg' 
+        borderWidth='1px' 
+        borderRadius='lg' 
+        overflow='hidden' 
+        boxShadow='lg'
+      >
+        <Center>
+          <Heading mb={6}>Sign in</Heading>
+        </Center>
+        <Input 
+          mb={4}
+          value={emailValue}
+          placeholder="email"
+          onChange={handleEmailChange} 
+        />
+        <Input
+          mb={4}
+          value={passwordValue}
+          placeholder="password"
+          type="password"
+          onChange={handlePasswordChange}
+        />
+        <Button 
+          colorScheme="purple"
+          onClick={handleLoginButtonPress}
+          isDisabled={!(emailValue && passwordValue)}
+        >
+          Login
+        </Button>
+      </Box>
+    </Center>
   )
 }
