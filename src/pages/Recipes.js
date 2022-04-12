c09a0b57620e732504ac99c98a0043d5078e6e61
Plsr <christianpoplawski@gmail.com>
2022-04-12 21:14:48 +0200

    Recipe submit happy path

--- src/pages/Recipes.js
+++ src/pages/Recipes.js
@@ -1,18 +1,34 @@
 import { Button, Heading } from '@chakra-ui/react';
 import { SmallAddIcon } from '@chakra-ui/icons';
 import styled from '@emotion/styled';
-import { useState } from 'react'
+import { useState, useContext, useEffect } from 'react'
 
 import RecipeForm from '../components/RecipeForm';
+import { postRecipe, getRecipes as getApiRecipes } from '../util/api';
+import UserContext from '../util/userContext';
 
 export default function Recipes() {
   const [showRecipeForm, setShowRecipeForm] = useState(false);
+  const [recipes, setRecipes] = useState([])
+  const user = useContext(UserContext);
+
+  useEffect(() => {
+    const getRecipes = async() => {
+      const fetchedRecipes = await getApiRecipes(user.accessToken);
+      setRecipes([...fetchedRecipes]);
+    }
+
+    getRecipes()
+  }, [])
 
   const handleAddButtonClick = () => {
     setShowRecipeForm(true);
   }
 
-  const handleFormSubmit = (data) => {
+  const handleFormSubmit = async (data) => {
+    const recipe = await postRecipe(user.accessToken, data)
+    setRecipes([...recipes, recipe])
+    setShowRecipeForm(false);
     console.log("Submitted form with data: ", data)
   }
 
@@ -20,13 +36,18 @@ export default function Recipes() {
     <>
       <HeaderWrapper>
         <Heading size="lg">Your Recipes</Heading>
-        <Button onClick={handleAddButtonClick} colorScheme='teal' leftIcon={<SmallAddIcon />}>Add Recipe</Button>
+        { !showRecipeForm && (
+          <Button onClick={handleAddButtonClick} colorScheme='teal' leftIcon={<SmallAddIcon />}>Add Recipe</Button>
+        )}
       </HeaderWrapper>
       { showRecipeForm && (
         <RecipeForm
           onClose={() => setShowRecipeForm(false)}
           onSubmit={handleFormSubmit}
         />
+      )}
+      {recipes.map(recipe =>
+        <p>{ recipe.title }</p>
       )}
     </>
   )
--- src/util/api.js
+++ src/util/api.js
@@ -1,3 +1,10 @@
+/**
+ * TODO:
+ * GENERAL TODOS FOR THIS FILE:
+ * - Use context to get user token instead of having to pass it in with every request
+ * - use api base class that sets the headers and so on?
+ */
+
 const SERVER_BASE_URL = process.env.REACT_APP_SERVER_BASE_URL
 
 export async function validateToken(token) {
@@ -13,4 +20,35 @@ export async function validateToken(token) {
   const json = await res.json()
   console.log(json)
   return json
+}
+
+// TODO: Error handling
+export async function postRecipe(token, reicpe) {
+  const requestOptions = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${token}`
+    },
+    body: JSON.stringify(reicpe)
+  }
+
+  const res = await fetch(SERVER_BASE_URL + '/recipes', requestOptions)
+  const json = await res.json()
+  console.log(json)
+  return json.recipe
+}
+
+export async function getRecipes(token) {
+  const requestOptions = {
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${token}`
+    }
+  }
+
+  const res = await fetch(SERVER_BASE_URL + '/recipes', requestOptions)
+  const json = await res.json()
+  console.log(json.recipes)
+  return json.recipes
 }
