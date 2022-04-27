18d742bb04a89fdd2101b16c4e9d06501d28a1d3
Plsr <christianpoplawski@gmail.com>
2022-04-27 22:10:07 +0200

    Distinct component for ingredientslist

---
+++ src/components/IngredientsList.js
@@ -0,0 +1,39 @@
+import { Box } from '@chakra-ui/react';
+
+export default function IngredientsList({ ingredients, maxIngredients }) {
+  const displayIngredients = ingredients.slice(0, maxIngredients)
+  const remainingIngredients = ingredients.length - maxIngredients
+  return (
+    <>
+      {displayIngredients.map((ingredient, index) => (
+        <>
+          <Box
+            key={index}
+            fontWeight="regular"
+            color="gray.700"
+            mb="1"
+          >
+            {ingredient.name}
+            <Box
+              key={index}
+              fontWeight="semibold"
+              color="gray.400"
+              display="inline"
+              pl="1"
+            >
+              {ingredient.amount}{ingredient.unit}
+            </Box>
+          </Box>
+        </>
+      ))}
+      <Box
+        fontWeight="regular"
+        color="gray.500"
+        mb="1"
+        fontSize="sm"
+      >
+        + {remainingIngredients} more
+      </Box>
+    </>
+  );
+}
--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,13 +1,8 @@
 import { Box, Heading } from '@chakra-ui/react';
 import { format } from 'date-fns';
-
-const MAX_INGREDIENTS = 3;
+import IngredientsList from './IngredientsList';
 
 export default function Recipe({ name, icon, ingredients, createdAt }) {
-  const displayIngredients = ingredients.slice(0, MAX_INGREDIENTS)
-  console.log(displayIngredients)
-  const remainingIngredients = ingredients.length - MAX_INGREDIENTS
-  console.log(remainingIngredients)
   return(
     <Box borderWidth='1px' borderRadius='lg' overflow='hidden' p="4">
       <Heading size='md' mb="1.5">{ icon } { name }</Heading>
@@ -21,37 +16,7 @@ export default function Recipe({ name, icon, ingredients, createdAt }) {
       >
         Added {format(new Date(createdAt), 'MMM dd, yyyy')}
       </Box>
-      {displayIngredients.map((ingredient, index) =>
-        <>
-          <Box
-            key={index}
-            fontWeight='regular'
-            color='gray.700'
-            mb="1"
-          >
-            { ingredient.name }
-          <Box
-            key={index}
-            fontWeight='semibold'
-            color='gray.400'
-            display="inline"
-            pl="1"
-          >
-            {ingredient.amount}{ingredient.unit}
-          </Box>
-          </Box>
-        </>
-      )}
-      { remainingIngredients > 0 && (
-        <Box
-          fontWeight='regular'
-          color='gray.500'
-          mb="1"
-          fontSize="sm"
-        >
-          + { ingredients.length - 3 } more
-        </Box>
-      )}
+      <IngredientsList ingredients={ingredients} maxIngredients={3} />
     </Box>
   )
 }
